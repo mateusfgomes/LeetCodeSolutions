c92cf03ef970747a45d6e860e707f2da194fcab4
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2022-10-03 21:54:29 -0300

    Uploading binary search

---
+++ Easy/binary_search.cpp
@@ -0,0 +1,15 @@
+int binarySearch(vector<int>& nums, int target, int start, int end){
+    if(start > end) return -1;
+    
+    int mid = (int) (start+end)/ 2;
+    if(nums[mid] == target) return mid;
+    
+
+    if(nums[mid] > target){
+        return binarySearch(nums, target, start, mid-1);
+    }
+    else{
+        return binarySearch(nums, target, mid+1, end);
+    }
+    
+}
